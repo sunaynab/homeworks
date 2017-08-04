c035fad37afa0fa53277a2fa929e60587dddd8de
sunaynab <sunaynabhikha@gmail.com>
2017-08-04 05:05:42 -0700

    Add map class

--- W1D5/W1D5_homework.rb
+++ W1D5/W1D5_homework.rb
@@ -38,4 +38,28 @@ class Map
   def initialize
     @map = []
   end
+
+  def assign(key, value)
+    @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+  end
+
+  def remove(key)
+    @map.reject! do |pair|
+      pair[0] == key
+    end
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
 end
