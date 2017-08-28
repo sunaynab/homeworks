be3d144626ac38170f4a1c7d2c79e0cfa80bf05f
sunaynab <sunaynabhikha@gmail.com>
2017-08-28 07:49:13 -0700

    Add W5D1 homework

---
+++ W5D1/music_app copy/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if user.nil?
+      flash.now[:errors] = 'Email or password does not match.'
+      render :new
+    else
+      login!(user)
+      redirect_to user_url
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
+    redirect_to new_session_url
+
+  end
+
+end
---
+++ W5D1/music_app copy/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: 'Password can\'t be blank.'}
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    token = SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
+end
